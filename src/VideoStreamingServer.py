d5186d35710e3376337586ed8c4930446848d0ff
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2025-04-21 01:03:02 +0200

    feat: implement methods of VideoStreamingServer class

--- src/VideoStreamingServer.py
+++ src/VideoStreamingServer.py
@@ -1,13 +1,29 @@
 from cv2.typing import MatLike
+from mjpeg_streamer import MjpegServer, Stream
 
 
 class VideoStreamingServer:
-	def __init__(self, host: str = "localhost", port: int = 8080) -> None: ...
+	def __init__(self, host: str = "localhost", port: int = 8080) -> None:
+		self.server: MjpegServer = MjpegServer(host, port)
+		self.streams: dict[str, Stream] = {}
 
-	def add_stream(self, stream_id: str) -> None: ...
+	def add_stream(self, stream_id: str) -> None:
+		if stream_id in self.streams:
+			raise ValueError(f'Stream with ID "{stream_id}" already exists.')
 
-	def update_stream(self, stream_id: str, frame: MatLike) -> None: ...
+		stream: Stream = Stream(stream_id)
 
-	def start(self) -> None: ...
+		self.server.add_stream(stream)
+		self.streams[stream_id] = stream
 
-	def stop(self) -> None: ...
+	def update_stream(self, stream_id: str, frame: MatLike) -> None:
+		if stream_id not in self.streams:
+			raise ValueError(f'Stream with ID "{stream_id}" does not exist.')
+
+		self.streams[stream_id].set_frame(frame)
+
+	def start(self) -> None:
+		self.server.start()
+
+	def stop(self) -> None:
+		self.server.stop()
