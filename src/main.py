9b5ad5d25ba7903b45ab9c86e17f8894f4145e43
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2025-04-23 21:55:52 +0200

    chore: update licensing notice in main.py file header

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 # Copyright (c) 2025 Nico-Gabriel Ruckendorfer
 #
 # This software is released under the MIT License.
-# For more information, see the LICENSE file.
+# For more information, see the LICENSE file in the root directory of this project.
 
 
 def main() -> None: ...
