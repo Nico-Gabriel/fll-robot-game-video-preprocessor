d23e4e53ea16aac19867a69bee15031fd9b2b21e
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2025-04-23 22:51:03 +0200

    chore: add copyright and licensing notice to video_streaming_server.py file header

--- src/video_streaming_server.py
+++ src/video_streaming_server.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2025 Nico-Gabriel Ruckendorfer
+#
+# This software is released under the MIT License.
+# For more information, see the LICENSE file in the root directory of this project.
+
+
 from cv2.typing import MatLike
 from mjpeg_streamer import MjpegServer, Stream
 
