3f3be698e3a29c1d14c69ca882ac088c9f5d91d9
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2025-04-23 21:48:20 +0200

    chore: add copyright and licensing notice to main.py file header

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+# Copyright (c) 2025 Nico-Gabriel Ruckendorfer
+#
+# This software is released under the MIT License.
+# For more information, see the LICENSE file.
+
 
 def main() -> None: ...
 
