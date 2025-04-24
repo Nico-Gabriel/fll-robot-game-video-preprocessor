38d243d3a8efde5bb21b8da350f677ef9d33bdae
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2025-04-24 19:28:16 +0200

    feat: create files video_processor.py, video_preprocessor.py, and robot_detector.py with class definitions (& file header including copyright and license notice)

---
+++ src/robot_detector.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2025 Nico-Gabriel Ruckendorfer
+#
+# This software is released under the MIT License.
+# For more information, see the LICENSE file in the root directory of this project.
+
+
+from cv2.typing import MatLike
+
+
+class RobotDetector:
+	def detect_robot(self, frame: MatLike) -> tuple[int, int, int, int]: ...
---
+++ src/video_preprocessor.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2025 Nico-Gabriel Ruckendorfer
+#
+# This software is released under the MIT License.
+# For more information, see the LICENSE file in the root directory of this project.
+
+
+class VideoPreprocessor: ...
